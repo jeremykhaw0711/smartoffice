e8e9bf910b10fd662ddfa1ace1914ef1293e9b0f
jeremykhaw0711 <102172233+jeremykhaw0711@users.noreply.github.com>
2022-04-02 17:50:44 +0800

    Add files via upload

--- dynamodb.js
+++ dynamodb.js
@@ -9,7 +9,7 @@ var getdevicedetails = (deviceid)=>{
     fetch(
         'https://mbvwh69009.execute-api.us-east-1.amazonaws.com/GetDevice/getdevice',
         {
-            method:"GET",
+            method:"POST",
             body: JSON.stringify({
                 "dev_id": deviceid
             }),
